aaff32a64a746969254df26a7808130e39038f8d
吴灏 <wuh@cnlemon.net>
2020-10-26 22:07:11 +0800

    基础项目配置

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# FE-wuhao-react-ts-components
 基于ts的react组件库
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: 吴灏
+ * @Date: 2020-10-26 21:56:19
+ * @LastEditors: 吴灏
+ * @LastEditTime: 2020-10-26 21:58:45
+ * @Description: file content
+ */
+// eslint-disable-next-line spaced-comment
+/// <reference types="react-scripts" />
